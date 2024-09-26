d7e2affae031a5e9c73125cb0e05fcd7e6caa80d
MeoKool <truongsyquang1202@gmail.com>
2024-09-27 04:18:42 +0700

    device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_mentor/splash.dart';
 
 void main() async {
+  // Ensure that widget binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+
+  // Run the app with DevicePreview for non-release modes
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       color: Colors.white,
-      home: SplashPage(),
+      home: const SplashPage(),
       debugShowCheckedModeBanner: false,
     );
   }
